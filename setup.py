8c056fa390e8dfda838a01857aaeb5be973cacbd
L. Scott Blankenship <pcxtsbl@192.168.1.107>
2023-01-26 18:44:59 +0000

    installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='worktools',
+    version='0.1.0',
+    description='Some tools for plotting and more for my workflow',
+    url='https://github.com/sblanky/worktools',
+    install_requires=[],
+    author='L. Scott Blankenship'
+    author_email='leo.blankenship1@nottingham.ac.uk',
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points={
+
+    }
+)
