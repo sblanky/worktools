2960663b7dd4b2fa6b93d6dd64100db6604bb058
L. Scott Blankenship <pcxtsbl@192.168.1.109>
2023-02-17 17:42:34 +0000

    curvature removing possible

--- worktools/deconvolution/da_standard.py
+++ worktools/deconvolution/da_standard.py
@@ -14,6 +14,7 @@ isotherm = pgp.isotherm_from_aif(
 
 unfiltered_results = {}
 
+
 def list_split(
     List, min_length
 ):
@@ -28,38 +29,120 @@ def list_split(
 def split_isotherm(
     isotherm,
     p_limits: tuple[float, float] = [None, None],
-    num_points = 10,
+    num_points: int = 10,
 ):
-    pressures = list(isotherm.pressure())
-    pressure = [x for x in pressures if x > p_limits[0]]
-    pressure = [x for x in pressures if x < p_limits[1]]
-    """
-    loading = []
-    for p in pressure:
-        loading.append(float(isotherm.loading_at(p)))
-        """
-    split_pressures = list(list_split(
+    pressure_full = list(isotherm.pressure())
+    pressure = [x for x in pressure_full if x > p_limits[0]]
+    pressure = [x for x in pressure if x < p_limits[1]]
+    pressure_lists = list(list_split(
         pressure, num_points
     )
     )
-    for split in split_pressures:
-        split_loading = []
-        for p in split:
-            split_loading.append(float(isotherm.loading_at(p)))
+    linear_pressure_lists = []
+    for pressure_list in pressure_lists:
+        loading_list = []
+        for p in pressure_list:
+            loading_list.append(float(isotherm.loading_at(p)))
+        log_p_exp = np.flip(
+            pgc.dr_da_plots.log_p_exp(
+                list(pressure_list), 2
+            )
+        )
+        log_v_adj = np.flip(
+            pgc.dr_da_plots.log_v_adj(
+                list(loading_list)
+            )
+        )
         try:
-            spline = interp1d(
-                pgc.dr_da_plots.log_p_exp(list(split), 2), 
-                pgc.dr_da_plots.log_v_adj(list(split_loading)),
-                assume_sorted=False
+            spline = PchipInterpolator(
+                log_p_exp, log_v_adj,
             )
         except ValueError as e:
             print(e)
             continue
-        plt.plot(split, spline(split))
-        plt.show()
-        second_derivative = spline.derivative(n=2)
-        plt.plot(split, second_derivative(split))
-        plt.show()
+        second_derivative = spline.derivative(nu=2)
+
+        second_derivative_range = abs(
+            second_derivative(min(log_p_exp)) - second_derivative(max(log_p_exp))
+        )
+        #print(second_derivative_range)
+
+        if second_derivative_range < 1e-3:
+            linear_pressure_lists.append(pressure_list)
+
+    return linear_pressure_lists
 
 
-split_isotherm(isotherm, [1e-5, 0.1])
+def isotherm_splitter(
+    original_isotherm = None,
+    pressure: list = None,
+):
+    loading = list() 
+    for p in pressure:
+        loading.append(
+            original_isotherm.loading_at(p)
+        )
+
+    return pg.PointIsotherm(
+        pressure=pressure,
+        loading=loading,
+
+        material=isotherm.material,
+        adsorbate=original_isotherm.adsorbate,
+        temperature=original_isotherm.temperature,
+
+        pressure_mode=original_isotherm.pressure_mode,
+        pressure_unit=original_isotherm.pressure_unit,
+        loading_basis=original_isotherm.loading_basis,
+        loading_unit=original_isotherm.loading_unit,
+        material_basis=original_isotherm.material_basis,
+        material_unit=original_isotherm.material_unit,
+    )
+
+
+def dr_plotter(
+    linear_pressure_lists: list,
+    isotherm,
+):
+    results_dict = {}
+    n=0
+    for pressure_list in linear_pressure_lists:
+        results = pgc.dr_da_plots.dr_plot(
+            isotherm,
+            p_limits=[min(pressure_list), max(pressure_list)],
+        )
+        results['length'] = len(pressure_list)
+        results['pressure'] = pressure_list
+        results_dict[n] = results
+        n+=1
+
+    return results_dict
+
+
+def dr_results_sorter(
+    results_dict
+):
+    results_filtered = {}
+    for r in results_dict:
+        result = results_dict[r]
+        if abs(result['corr_coef']) > 0.999:
+            results_filtered[r] = results_dict[r]
+
+    return results_filtered
+
+
+linear_pressure_lists = split_isotherm(
+    isotherm, p_limits=[1e-5, 0.1]
+)
+print(linear_pressure_lists)
+results_dict = dr_plotter(linear_pressure_lists, isotherm)
+filtered_results = dr_results_sorter(results_dict)
+for f in filtered_results:
+    filt = filtered_results[f]
+    print(filt['pressure'])
+    pgc.dr_da_plots.dr_plot(
+        isotherm,
+        p_limits=[min(filt['pressure']), max(filt['pressure'])],
+        verbose=True,
+    )
+    plt.show()
